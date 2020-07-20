d68c82626fc7c305baa12a65a770dd6b87afa887
albieduffy <albie.duffy@gmail.com>
2020-07-20 11:21:02 +0100

    Change port

--- js/app.js
+++ js/app.js
@@ -6,8 +6,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.text());
 
-const port = 3000;
-
 const excuses = { shopping: {morning: ["bags split on the way back" , 2, 3], afternoon: [1, 2, 3], evening: [1, 2 ,3]}, working: {morning: [1 , 2, 3], afternoon: [1, 2, 3], evening: [1, 2 ,3]}, kids: {morning: [1 , 2, 3], afternoon: [1, 2, 3], evening: [1, 2 ,3]}};
 
 app.get('/', (req, res) => res.send('Need an excuse?'));
@@ -16,4 +14,4 @@ app.get('/excuses/:action/:timing', (req, res) => {
     res.send(JSON.stringify(excuses[req.params.action][req.params.timing][Math.floor(Math.random()*3)]));
 });
 
-app.listen(port, () => console.log(`Express departing now from http://localhost:${port}`));
+app.listen(process.env.PORT || 3000, () => console.log(`Express departing now from http://localhost:${port}`));
